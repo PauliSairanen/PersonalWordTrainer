0bb0c16ee9f345107428fdf2f02ab1e9a27b8e3a
Paul <pauli.sairanen@gmail.com>
2020-12-09 12:22:06 +0200

    Initial Commit

---
+++ Personal Word Trainer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Personal Word Trainer
+//
+//  Created by Pauli Sairanen on 9/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
