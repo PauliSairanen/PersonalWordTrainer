6db814aba14cb5b8ffc0a68e3bc03f67c470a521
Paul <pauli.sairanen@gmail.com>
2021-01-05 10:25:57 +0200

    Deleted unused code, after the use of IQKeyboardManager

--- Controllers/GameViewController.swift
+++ Controllers/GameViewController.swift
@@ -29,31 +29,11 @@ class GameViewController: UIViewController {
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// call the 'keyboardWillShow' function when the view controller receive notification that keyboard is going to be shown
-//		NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(notification:)), name: UIResponder.keyboardWillShowNotification, object: nil)
-//		NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(notification:))), name: UIResponder.keyboardWillHideNotification, object: nil)
-		
-
-	
-	
-		
 		loadItems()
 		resetGame()
 	}
 	
-	@objc func keyboardWillShow(notification: NSNotification) {
-		guard let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue else {
-			// if keyboard size is not available for some reason, dont do anything
-			return
-		}
-		// move the root view up by the distance of keyboard height
-		self.view.frame.origin.y = 0 - keyboardSize.height
-	}
-	
-	@objc func keyboardWillHide(notification: NSNotification) {
-		// move back the root view origin to zero
-		self.view.frame.origin.y = 0
-	}
+
 	
 	
 	
@@ -164,6 +144,8 @@ class GameViewController: UIViewController {
 	
 	
 	
+	
+	
 	//MARK: - Data manipulation methods (Save, Read)
 	func saveItems() {
 		do {
@@ -190,4 +172,10 @@ class GameViewController: UIViewController {
 			print("Error fetching data from context \(error)")
 		}}
 	
+}
+
+extension GameViewController {
+	open override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+		self.view.endEditing(true)
+	}
 }
