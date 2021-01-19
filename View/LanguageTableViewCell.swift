0918f5648cb5f12ced8d6967af372654f035700e
Paul <pauli.sairanen@gmail.com>
2021-01-19 13:11:24 +0200

    Tried to add box shadow to TableViewCells. Commented out for now

--- View/LanguageTableViewCell.swift
+++ View/LanguageTableViewCell.swift
@@ -18,6 +18,18 @@ class LanguageTableViewCell: UITableViewCell {
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+		
+//		backgroundColor = .clear
+//		layer.masksToBounds = false
+//		layer.shadowOpacity = 0.23
+//		layer.shadowRadius = 4
+//		layer.shadowOffset = CGSize(width: 0, height: 0)
+//		layer.shadowColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+//		
+//		contentView.backgroundColor = .white
+//		contentView.layer.cornerRadius = 8
+		
+		
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
