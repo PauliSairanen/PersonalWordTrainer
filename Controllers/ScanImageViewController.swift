5bb869862e3d4785fdc58a3db70317a129dbae5d
Paul <pauli.sairanen@gmail.com>
2021-01-19 11:28:50 +0200

    Added ScanImageView Controller file

---
+++ Controllers/ScanImageViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ScanImageViewController.swift
+//  Personal Word Trainer
+//
+//  Created by Pauli Sairanen on 14/1/21.
+//
+
+import UIKit
+
+class ScanImageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
